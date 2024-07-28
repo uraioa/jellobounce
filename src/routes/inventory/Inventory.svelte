5894f35889f31d642f072a0b343b6f292fcf5ef4
lucas <manlucas603@proton.me>
2024-07-28 21:43:38 +0300

    added jellobounce logo to inventory

--- src/routes/inventory/Inventory.svelte
+++ src/routes/inventory/Inventory.svelte
@@ -1,10 +1,30 @@
-<img class="watermark" src="img/lb-logo.svg" alt="watermark">
+<script lang="ts">
+    import {fade} from "svelte/transition";
+</script>
 
-<style>
-    .watermark {
+<div class="logo" transition:fade={{duration: 200}}>jellobounce</div>
+<div class="shaderfix" transition:fade={{duration: 200}}></div>
+
+<style lang="scss">
+    @import "../../colors.scss";
+
+    .logo {
         position: absolute;
-        bottom: 15px;
-        right: 15px;
-        width: 165px;
+        left: 15px;
+        top: 15px;
+        font-family: Borel;
+        color: rgba(white, 1);
+        font-size: 50px;
+        text-shadow: 0px 0px 10px rgba(black, 0.3);
+        z-index: 1;
+        text-align: center;
+    }
+
+    .shaderfix {
+        position: absolute;
+        background-color: rgba(black, 0.1);
+        height: 100vw;
+        width: 100vw;
+        z-index: -1;
     }
 </style>
